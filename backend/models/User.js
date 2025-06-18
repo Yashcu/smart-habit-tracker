c43d0f843130967477b1b45fa3394c2ef624ff99
Yashcu <22BCS13420@cuchd.in>
2025-06-18 22:38:26 +0530

    Day 2 : Backend User authentication with jwt

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const moongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new moongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = moongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const moongoose = require('mongoose');
 const cors = require('cors');
-const e = require('express');
+const connectDB = require('./config/dg.js');
 
 dotenv.config();
+connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', require('./routes/authRoutes'));
+
 app.get('/', (req,res) => res.send('API is running...'));
 
-moongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
 
